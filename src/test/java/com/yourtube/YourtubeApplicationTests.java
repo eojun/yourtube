42d9b4b433c4895f6353c3bad5fecbcb79b87dea
eojunhwang <eojun23@gmail.com>
2021-11-16 00:23:06 +0900

    Initial commit

---
+++ src/test/java/com/yourtube/YourtubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yourtube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourtubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
